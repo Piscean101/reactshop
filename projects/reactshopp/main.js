6af4533aa31fedbfa591cc3b97eedf4c7ea01d57
Deondre <deondre_palmer@hotmail.com>
2023-05-08 19:15:35 -0400

    Inventory Complete!

--- projects/reactshopp/main.js
+++ projects/reactshopp/main.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const cookie = require('cookie-parser');
 const express = require('express');
+const session = require('express-session');
 const app = express();
 const path = require('path');
 const PORT = '5174';
@@ -10,6 +11,18 @@ app.listen(PORT, () => {
 });
 app.use(cookie());
 app.use(express.static(path.join(__dirname, 'src')));
+app.use(express.urlencoded({ extended: true }));
+app.use(session({ 
+    secret: "bbw",
+    resave: true,
+    saveUninitialized: true,
+    rolling: true,
+    cookie: {
+        secure: false,
+        maxAge: 1800000,
+        minAge: 300000
+    }
+}));
 const mysql = require('mysql');
 const db = mysql.createConnection({
     host: 'localhost',
@@ -25,6 +38,190 @@ db.connect((err) => {
         console.log(err)
     }
 });
-app.get('/search', (req, res) => {
-    res.render('search')
+app.get('/', (req, res) => {
+    db.query(`SELECT * FROM inventory ORDER BY 3`, (err, data) => {
+        for (let i = 0 ; i < data.length ; i ++) {
+            if (data[i].onsale) {
+                data[i].saleprice = Math.ceil(data[i].cost - ((data[i].cost * data[i].onsale)/100));
+            }
+        }
+        res.render('search' , {
+            data: data
+        })
+    })
+})
+app.get('/addCart/:id', (req, res) => {
+    res.redirect('/');
+})
+app.get('/home', (req, res) => {
+    res.sendFile('index.html', { root: __dirname })
+})
+
+// onsale = integer
+// order = 'L2H' or 'H2L'
+// category = 'string'
+app.get('/filter', (req, res) => {
+    if (req.query.onsale && !req.query.order && !req.query.category) {
+        db.query(`SELECT * FROM inventory WHERE onsale IS NOT NULL`, (err, data) => {
+            for (let i = 0 ; i < data.length ; i ++) {
+                if (data[i].onsale) {
+                    data[i].saleprice = Math.ceil(data[i].cost - ((data[i].cost * data[i].onsale)/100));
+                }
+            }
+            res.render('search' , {
+                data: data
+            })
+        })
+    }
+    // <- SALE N.ORDER N.CAT
+    // SALE N.ORDER CAT ->
+    else if (req.query.onsale && !req.query.order && req.query.category) {
+        db.query(`SELECT * FROM inventory WHERE onsale IS NOT NULL AND party = '${req.query.category}'`, (err, data) => {
+            for (let i = 0 ; i < data.length ; i ++) {
+                if (data[i].onsale) {
+                    data[i].saleprice = Math.ceil(data[i].cost - ((data[i].cost * data[i].onsale)/100));
+                }
+            }
+            res.render('search' , {
+                data: data
+            })
+        })
+    } 
+    // <- SALE N.ORDER CAT
+    // SALE ORDER N.CAT -> 
+    else if (req.query.onsale && req.query.order && !req.query.category) {
+        if (req.query.order == 'L2H') {
+        db.query(`SELECT * FROM inventory WHERE onsale IS NOT NULL ORDER BY 4`, (err, data) => {
+            for (let i = 0 ; i < data.length ; i ++) {
+                if (data[i].onsale) {
+                    data[i].saleprice = Math.ceil(data[i].cost - ((data[i].cost * data[i].onsale)/100));
+                }
+            }
+            res.render('search' , {
+                data: data
+            })
+        })
+    } else {
+        db.query(`SELECT * FROM inventory WHERE onsale IS NOT NULL ORDER BY 4 DESC`, (err, data) => {
+            for (let i = 0 ; i < data.length ; i ++) {
+                if (data[i].onsale) {
+                    data[i].saleprice = Math.ceil(data[i].cost - ((data[i].cost * data[i].onsale)/100));
+                }
+            }
+            res.render('search' , {
+                data: data
+            })
+        })
+    }
+    }
+    // <- SALE ORDER N.CAT
+    // SALE ORDER CAT ->
+    else if (req.query.onsale && req.query.order && req.query.category) {
+        if (req.query.order == 'L2H') {
+        db.query(`SELECT * FROM inventory WHERE onsale IS NOT NULL AND party = '${req.query.category}' ORDER BY 4`, (err, data) => {
+            for (let i = 0 ; i < data.length ; i ++) {
+                if (data[i].onsale) {
+                    data[i].saleprice = Math.ceil(data[i].cost - ((data[i].cost * data[i].onsale)/100));
+                }
+            }
+            res.render('search' , {
+                data: data
+            })
+        })
+    } else {
+        db.query(`SELECT * FROM inventory WHERE onsale IS NOT NULL AND party = '${req.query.category}' ORDER BY 4 DESC`, (err, data) => {
+            for (let i = 0 ; i < data.length ; i ++) {
+                if (data[i].onsale) {
+                    data[i].saleprice = Math.ceil(data[i].cost - ((data[i].cost * data[i].onsale)/100));
+                }
+            }
+            res.render('search' , {
+                data: data
+            })
+        })
+    }
+    } 
+    // <- SALE ORDER CAT 
+      // N.SALE ORDER CAT ->
+      else if (!req.query.onsale && req.query.order && req.query.category) {
+        if (req.query.order == 'L2H') {
+        db.query(`SELECT * FROM inventory WHERE onsale IS NULL AND party = '${req.query.category}' ORDER BY 4`, (err, data) => {
+            for (let i = 0 ; i < data.length ; i ++) {
+                if (data[i].onsale) {
+                    data[i].saleprice = Math.ceil(data[i].cost - ((data[i].cost * data[i].onsale)/100));
+                }
+            }
+            res.render('search' , {
+                data: data
+            })
+        })
+    } else {
+        db.query(`SELECT * FROM inventory WHERE onsale IS NULL AND party = '${req.query.category}' ORDER BY 4 DESC`, (err, data) => {
+            for (let i = 0 ; i < data.length ; i ++) {
+                if (data[i].onsale) {
+                    data[i].saleprice = Math.ceil(data[i].cost - ((data[i].cost * data[i].onsale)/100));
+                }
+            }
+            res.render('search' , {
+                data: data
+            })
+        })
+    }
+    } 
+    // <- N.SALE ORDER CAT 
+    // ORDER ->
+    else if (req.query.order) {
+        if (req.query.order == 'L2H') {
+        db.query(`SELECT * FROM inventory ORDER BY 4`, (err, data) => {
+            for (let i = 0 ; i < data.length ; i ++) {
+                if (data[i].onsale) {
+                    data[i].saleprice = Math.ceil(data[i].cost - ((data[i].cost * data[i].onsale)/100));
+                }
+            }
+            res.render('search' , {
+                data: data
+            })
+        })
+    } else {
+        db.query(`SELECT * FROM inventory ORDER BY 4 DESC`, (err, data) => {
+            for (let i = 0 ; i < data.length ; i ++) {
+                if (data[i].onsale) {
+                    data[i].saleprice = Math.ceil(data[i].cost - ((data[i].cost * data[i].onsale)/100));
+                }
+            }
+            res.render('search' , {
+                data: data
+            })
+        })
+    }
+    }
+    // <- ORDER
+    // CAT ->
+    else if (req.query.category) {
+        db.query(`SELECT * FROM inventory WHERE party = '${req.query.category}'`, (err, data) => {
+            for (let i = 0 ; i < data.length ; i ++) {
+                if (data[i].onsale) {
+                    data[i].saleprice = Math.ceil(data[i].cost - ((data[i].cost * data[i].onsale)/100));
+                }
+            }
+            res.render('search' , {
+                data: data
+            })
+        })
+    }
+    // <- CAT
+    else {
+        res.redirect('/');
+    }
+})
+
+
+
+
+
+
+
+
+app.get('*', (req, res) => {
+    res.send('We could not find the page you were looking for');
 })
